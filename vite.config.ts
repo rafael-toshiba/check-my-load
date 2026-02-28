51f6174f0dfed2a0aad140d04a6e605c2bb96635
Rafael Toshiba <dev.rafaelsantos@outlook.com>
2026-02-28 09:40:41 -0300

    chore: limpa referencias, plugins e meta tags do lovable

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -12,7 +11,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(), 
+    basicSsl()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
